816235643ba8b1ca83dbd8cd9469e5f93097a006
nxt964 <thanhnx964@gmail.com>
2026-01-04 03:56:57 +0700

    [Core-Service] Fix CORS bug from front-end

---
+++ core-service/src/main/java/com/tradebidz/core_service/core/config/WebConfig.java
@@ -0,0 +1,29 @@
+package com.tradebidz.core_service.core.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "http://localhost:5173",
+                                "http://localhost:3000",
+                                "http://127.0.0.1:5173",
+                                "http://127.0.0.1:3000")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
+                        .maxAge(3600);
+            }
+        };
+    }
+}
